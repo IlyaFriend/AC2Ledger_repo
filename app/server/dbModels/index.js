9daba60bbbe5a10d5441b5bccd8a4b3ce80e9282
IlyaFriend <eliasbell2233@gmail.com>
2024-04-07 09:04:01 +0300

    feat: add users model

---
+++ app/server/dbModels/index.js
@@ -0,0 +1,14 @@
+// Root file for combining and exporting all the models
+import { model } from 'mongoose'
+
+import UserSchema from './user'
+// import AchievementSchema from "./achievement"
+// import DepartmentSchema from "./department"
+// import FacultySchema from "./faculty"
+// import UniversitySchema from "./university"
+
+export const User = model('users', UserSchema)
+// export const Achievement = model("achievement", AchievementSchema)
+// export const Department = model("department", DepartmentSchema)
+// export const Faculty = model("faculty", FacultySchema)
+// export const University = model("university", UniversitySchema)
---
+++ app/server/dbModels/user.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose'
+import bcrypt from 'mongoose-bcrypt'
+
+const { Schema } = mongoose
+
+const SchemaMain = new Schema({
+  username: { type: String, unique: true, required: true },
+  password: { type: String, bcrypt: true, required: true },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  patronymic: String,
+  courses: [String],
+  externalProfiles: Object,
+  departments: [{
+    type: Schema.Types.ObjectId,
+    ref: 'department'
+  }],
+  achievements: [{
+    type: Schema.Types.ObjectId,
+    ref: 'achievement'
+  }]
+})
+
+SchemaMain.methods.verifyPasswordSync = function (password) {
+  return bcrypt.compareSync(password, this.password)
+}
+
+SchemaMain.virtual('fullName').get(() => {
+  return `${this.lastName} ${this.firstName} ${this.patronymic || ''}`.trim()
+})
+
+SchemaMain.plugin(bcrypt)
+
+export default SchemaMain
