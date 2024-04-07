b10ee110ed5895304af119d0b95b2ac37aab1058
IlyaFriend <eliasbell2233@gmail.com>
2024-04-07 09:25:50 +0300

    feat: add sign up form

--- app/pages/signup.vue
+++ app/pages/signup.vue
@@ -1,8 +1,36 @@
 <template>
-  <div class="p-8 border rounded">
-    <button @click="signup">
-      sign up
-    </button>
+  <div class="flex items-center justify-center h-screen">
+    <form class="px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md" @submit.prevent="signup">
+      <div class="mb-4">
+        <label class="block mb-2 text-sm font-bold text-gray-700" for="username">
+          Username
+        </label>
+        <input id="username" v-model="username" class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" type="text" placeholder="Username">
+      </div>
+      <div class="mb-6">
+        <label class="block mb-2 text-sm font-bold text-gray-700" for="password">
+          Password
+        </label>
+        <input id="password" v-model="password" class="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" type="password" placeholder="Password">
+      </div>
+      <div class="mb-4">
+        <label class="block mb-2 text-sm font-bold text-gray-700" for="firstName">
+          First name
+        </label>
+        <input id="firstName" v-model="firstName" class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" type="text" placeholder="First Name">
+      </div>
+      <div class="mb-4">
+        <label class="block mb-2 text-sm font-bold text-gray-700" for="firstName">
+          Last name
+        </label>
+        <input id="lastName" v-model="lastName" class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" type="text" placeholder="Last Name">
+      </div>
+      <div class="flex items-center justify-between">
+        <button class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline" type="submit">
+          Sign In
+        </button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -12,14 +40,19 @@ definePageMeta({
   auth: false
 })
 
+const username = ref('')
+const password = ref('')
+const firstName = ref('')
+const lastName = ref('')
+
 async function signup () {
   const res = await $fetch('/api/users/', {
     method: 'POST',
     body: {
-      username: 'testuser1',
-      password: '11111111',
-      firstName: 'John',
-      lastName: 'Doe'
+      username: username.value,
+      password: password.value,
+      firstName: firstName.value,
+      lastName: lastName.value
     }
   })
   console.log('signup response:', res)
