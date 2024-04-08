27897992e06aca5476912e30cedee3feed38b7d8
IlyaFriend <eliasbell2233@gmail.com>
2024-04-08 23:46:07 +0300

    clean: index page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,38 +1,9 @@
 <template>
-  <div class="flex justify-center items-center h-screen">
-    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" @submit.prevent="login">
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-          Username
-        </label>
-        <input id="username" v-model="username" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Username">
-      </div>
-      <div class="mb-6">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-          Password
-        </label>
-        <input id="password" v-model="password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" type="password" placeholder="Password">
-      </div>
-      <div class="flex items-center justify-between">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-          Sign In
-        </button>
-      </div>
-    </form>
+  <div>
+    <LayoutHeader />
+    <h1>Welcome on the main page</h1>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, useAuth } from '#imports'
-const { signIn } = useAuth()
-
-const username = ref('')
-const password = ref('')
-
-function login () {
-  signIn({
-    username: username.value,
-    password: password.value
-  })
-}
 </script>
