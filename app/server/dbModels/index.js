737ecebea76fec73fce81f81fd8d941cd6b1b3fd
IlyaFriend <eliasbell2233@gmail.com>
2024-04-17 20:20:44 +0300

    chore: add achievement model to imports

--- app/server/dbModels/index.js
+++ app/server/dbModels/index.js
@@ -2,13 +2,13 @@
 import { model } from 'mongoose'
 
 import UserSchema from './user'
-// import AchievementSchema from "./achievement"
-// import DepartmentSchema from "./department"
-// import FacultySchema from "./faculty"
-// import UniversitySchema from "./university"
+import AchievementSchema from './achievement'
+// import DepartmentSchema from './department'
+// import FacultySchema from './faculty'
+// import UniversitySchema from './university'
 
 export const User = model('users', UserSchema)
-// export const Achievement = model("achievement", AchievementSchema)
-// export const Department = model("department", DepartmentSchema)
-// export const Faculty = model("faculty", FacultySchema)
-// export const University = model("university", UniversitySchema)
+export const Achievement = model('achievement', AchievementSchema)
+// export const Department = model('department', DepartmentSchema)
+// export const Faculty = model('faculty', FacultySchema)
+// export const University = model('university', UniversitySchema)
