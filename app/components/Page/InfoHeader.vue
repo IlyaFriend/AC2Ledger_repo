55ab1345c98f67a94a00f7ed4e159950484e5dfe
IlyaFriend <eliasbell2233@gmail.com>
2024-04-21 16:20:30 +0300

    feat: add info-header component

---
+++ app/components/Page/InfoHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bg-white rounded-lg">
+    <h2 class="text-3xl font-semibold">
+      {{ title }}
+    </h2>
+    <p v-if="description" class="text-gray-500 italic">
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    default: ''
+  }
+})
+</script>
