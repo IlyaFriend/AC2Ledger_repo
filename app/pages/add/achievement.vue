e5c7bcbd183294779ca79dcafcd013f77a15f000
IlyaFriend <eliasbell2233@gmail.com>
2024-04-21 16:16:30 +0300

    feat: add achievements

--- app/pages/add/achievement.vue
+++ app/pages/add/achievement.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    <LayoutHeader />
     <h1>Add achievement</h1>
     <div class="flex justify-center">
-      <DynamicForm />
+      <DynamicForm title="Add an achievement" @on-submit="achievementData => addAchievement(achievementData)" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+const addAchievement = async (achievementData) => {
+  console.log(achievementData)
+  await $fetch('/api/achievements/', {
+    method: 'POST',
+    body: {
+      data: achievementData
+    }
+  })
+}
 </script>
