888ddb0b80776b5add9b7bd1649bc84dab5e160b
IlyaFriend <eliasbell2233@gmail.com>
2024-04-22 23:09:44 +0300

    feat: use fields as a prop in dynamic-form

--- app/components/DynamicForm.vue
+++ app/components/DynamicForm.vue
@@ -1,40 +1,55 @@
 <template>
-  <div class="w-[600px] border rounded p-4">
+  <div class="w-full border rounded p-4">
     <FormKit type="form" submit-label="Add" @submit="submitForm">
       <div v-if="title" class="text-xl font-semibold text-center mb-4">
         {{ title }}
       </div>
-      <div v-for="(field, key) in fields" :key="key" :label="field.label" class="flex justify-between items-center mb-4">
-        <FormKit outer-class="!max-w-[17%] $remove:mb-4" type="text" :value="field.name" @change="e => changeFieldName(key, e.target.value)" />
+      <div v-for="(field, key) in fieldsRef" :key="key" :label="field.label" class="flex justify-between items-center mb-4">
+        <FormKit v-if="!field.immutable" outer-class="!max-w-[17%] $remove:mb-4" type="text" :value="field.name" @change="e => changeFieldName(key, e.target.value)" />
+        <span v-else class="!max-w-[17%] w-full px-3">{{ field.name }}</span>
         <FormKit v-model="formData[key]" outer-class="!max-w-[77%] $remove:mb-4" type="text" :placeholder="field.placeholder" />
-        <Icon name="material-symbols-light:delete-outline-sharp" size="1.5em" class="cursor-pointer hover:text-primary-600" @click="deleteFieldName(key)" />
+        <button :disabled="field.immutable" class="cursor-pointer hover:text-primary-600 disabled:text-gray-300 disabled:cursor-default" @click="deleteFieldName(key)">
+          <Icon name="material-symbols-light:delete-outline-sharp" size="1.5em" />
+        </button>
       </div>
-      <button type="button" variant="info" class="mb-4 hover:text-primary-500" @click="addField">
-        <Icon name="material-symbols-light:add-circle-outline-rounded" size="2em" /> Add a new field
+      <button type="button" variant="info" class="mb-4 hover:text-primary-500 flex items-center" @click="addField">
+        <Icon name="material-symbols-light:add-circle-outline-rounded" size="2em" /><span class="ml-1">Add a new field</span>
       </button>
     </FormKit>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, type Ref } from 'vue'
+import { ref } from 'vue'
 import { useAuth } from '#imports'
 
-defineProps(['title'])
+const props = defineProps({
+  title: {
+    type: String,
+    default: ''
+  },
+  fields: {
+    type: Object,
+    default: () => {
+      return {
+        0: { name: 'title', label: 'Title', type: 'text', placeholder: 'Enter title', immutable: true },
+        1: { name: 'type', label: 'Type', type: 'text', placeholder: 'Enter type', immutable: true }
+      }
+    }
+  }
+})
 const emits = defineEmits(['on-submit'])
 const {
   data
 } = useAuth()
-const fields: Ref<any> = ref({
-  0: { name: 'title', label: 'Title', type: 'text', placeholder: 'Enter title' },
-  1: { name: 'type', label: 'Type', type: 'text', placeholder: 'Enter type' }
-})
 const formData = ref({})
 const indexKey = ref(2)
 
+const fieldsRef = ref(props.fields)
+
 const submitForm = () => {
   let achievementData: any = {}
-  for (const [key, value] of Object.entries(fields.value)) {
+  for (const [key, value] of Object.entries(fieldsRef.value)) {
     achievementData[value?.name] = formData.value[key]
   }
   const { title, type, ...details } = achievementData
@@ -54,15 +69,15 @@ const submitForm = () => {
 
 const addField = () => {
   const newField = { name: `key${indexKey.value - 1}`, label: '', type: 'text', placeholder: '' }
-  fields.value[indexKey.value] = newField
+  fieldsRef.value[indexKey.value] = newField
   indexKey.value++
 }
 
 const changeFieldName = (key: string, newFieldName: string) => {
-  fields.value[key].name = newFieldName
+  fieldsRef.value[key].name = newFieldName
 }
 
 const deleteFieldName = (key: string) => {
-  delete fields.value[key]
+  delete fieldsRef.value[key]
 }
 </script>
