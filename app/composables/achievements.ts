0e26a12f30b089f38e10ad917ff6a87da8ce323a
IlyaFriend <eliasbell2233@gmail.com>
2024-04-23 23:06:28 +0300

    feat: add delete-achievement composable

--- app/composables/achievements.ts
+++ app/composables/achievements.ts
@@ -7,3 +7,13 @@ export const createAchievement = async (achievementData) => {
   })
   return createdAchievement
 }
+
+export const deleteAchievement = async (achievementId: string) => {
+  if (!achievementId) {
+    return
+  }
+  const deletedAchievement = await $fetch(`/api/achievements/${achievementId}`, {
+    method: 'DELETE'
+  })
+  return deletedAchievement
+}
