d3d2a1dee1ae83d5787066082c29418ae8ef59d7
IlyaFriend <eliasbell2233@gmail.com>
2024-04-23 23:06:57 +0300

    feat: add dialog-action component

---
+++ app/components/DialogAction.vue
@@ -0,0 +1,99 @@
+<template>
+  <TransitionRoot appear :show="isOpen" as="template">
+    <Dialog as="div" class="relative z-10" @close="closeModal">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black/25" />
+      </TransitionChild>
+      <div class="fixed inset-0 overflow-y-auto">
+        <div
+          class="flex min-h-full items-center justify-center p-4 text-center"
+        >
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel
+              class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+            >
+              <div class="absolute top-0 right-0 p-4">
+                <CloseButton @click="closeModal" />
+              </div>
+              <DialogTitle
+                as="h3"
+                class="text-lg font-medium leading-6 text-gray-900"
+              >
+                {{ title }}
+              </DialogTitle>
+              <div class="mt-2">
+                <p class="text-sm text-gray-500">
+                  {{ description }}
+                </p>
+              </div>
+              <div class="mt-4">
+                <button
+                  type="button"
+                  class="inline-flex justify-center rounded-md border border-transparent bg-primary-100 px-4 py-2 text-sm font-medium text-primary-900 hover:bg-primary-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-500 focus-visible:ring-offset-2"
+                  @click="handleAction"
+                >
+                  {{ submitLabel }}
+                </button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
+
+<script setup lang="ts">
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  TransitionChild,
+  TransitionRoot
+} from '@headlessui/vue'
+
+const props = defineProps<{
+  title?: string
+  description?: string
+  submitLabel?: string
+  callback?:() => any | void
+}>()
+
+const isOpen = ref(false)
+
+function closeModal () {
+  isOpen.value = false
+}
+function openModal () {
+  isOpen.value = true
+}
+
+function handleAction () {
+    console.log('callback DialogAction')
+  if (props.callback) {
+    props.callback()
+  }
+  closeModal()
+}
+
+defineExpose({
+  openModal,
+  closeModal
+})
+</script>
