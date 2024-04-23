d3c92301afa3a91ad21c5d87e4e78c40b36ea0ca
IlyaFriend <eliasbell2233@gmail.com>
2024-04-23 21:02:32 +0300

    feat: improve get achievements route & add user param

--- app/server/api/achievements/index.get.ts
+++ app/server/api/achievements/index.get.ts
@@ -4,10 +4,20 @@ import { Achievement } from '~/server/dbModels'
 export default defineEventHandler(async (event) => {
   const query = getQuery(event)
   const createdBy = query?.createdBy
+  const user = query?.user
 
   try {
-    console.log(`*** GET /api/achievements/?createdBy=${createdBy} ***`)
-    const achievements = await Achievement.find({ createdBy })
+    console.log(`*** GET /api/achievements/?createdBy=${createdBy}&user=${user} ***`)
+
+    const filter: any = {}
+    if (createdBy) {
+      filter.createdBy = createdBy
+    }
+    if (user) {
+      filter.users = { $in: [user] }
+    }
+
+    const achievements = await Achievement.find(filter)
 
     console.log('achievements;', achievements)
     return achievements
