ddb4f1602597e2f6e665d28ac1e7252375c9d5bd
IlyaFriend <eliasbell2233@gmail.com>
2024-04-25 00:05:26 +0300

    feat: add a route for updating achievements

---
+++ app/server/api/achievements/[id].put.ts
@@ -0,0 +1,24 @@
+import { defineEventHandler, readBody } from 'h3'
+import { Achievement } from '../../dbModels'
+
+interface IRequestBody {
+  data: any;
+}
+export default defineEventHandler(async (event) => {
+  const { id } = event.context.params
+  console.log(`*** PUT /api/achievements/${id} ***`)
+  const { data } = await readBody<IRequestBody>(event)
+  try {
+    const updatedAchievement = await Achievement.findByIdAndUpdate(id, data)
+    console.log('updated achivement:', updatedAchievement, '\n')
+    return updatedAchievement
+    // return null
+  } catch (err) {
+    console.dir(err)
+    event.res.statusCode = 500
+    return {
+      code: 'ERROR',
+      message: 'Something wrong.'
+    }
+  }
+})
