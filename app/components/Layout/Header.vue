c83a1456397d5bbae6f1a148e9ec5228e80d6a90
IlyaFriend <eliasbell2233@gmail.com>
2024-04-26 00:44:26 +0300

    chore: use nuxtlinks instead of a-tags & change pathes

--- app/components/Layout/Header.vue
+++ app/components/Layout/Header.vue
@@ -2,10 +2,10 @@
   <header class="bg-white shadow-md">
     <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
       <div class="flex lg:flex-1">
-        <a href="#" class="-m-1.5 p-1.5">
+        <NuxtLink to="#" class="-m-1.5 p-1.5">
           <span class="sr-only">ScholarSphere</span>
           <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=cyan&shade=600" alt="ScholarSphere">
-        </a>
+        </NuxtLink>
       </div>
       <div class="flex lg:hidden">
         <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700  hover:text-primary-500" @click="mobileMenuOpen = true">
@@ -14,8 +14,12 @@
         </button>
       </div>
       <div class="hidden lg:flex lg:gap-x-12">
-        <a href="search/teacher" class="text-sm leading-6 text-gray-900 hover:text-primary-500"><Icon name="material-symbols-light:search" size="1.5em" />Search for a teacher</a>
-        <a v-for="item in navigation" :key="item.name" :href="item.href" class="text-sm leading-6 text-gray-900 hover:text-primary-500">{{ item.name }}</a>
+        <NuxtLink to="/teacher/search" class="text-sm leading-6 text-gray-900 hover:text-primary-500">
+          <Icon name="material-symbols-light:search" size="1.5em" />Search for a teacher
+        </NuxtLink>
+        <NuxtLink v-for="item in navigation" :key="item.name" :to="item.href" class="text-sm leading-6 text-gray-900 hover:text-primary-500">
+          {{ item.name }}
+        </NuxtLink>
       </div>
       <div class="hidden lg:flex lg:flex-1 lg:justify-end">
         <button v-if="status === 'authenticated'" class="text-sm leading-6 text-gray-900 hover:text-primary-500" @click="handleSignout">
@@ -30,10 +34,10 @@
       <div class="fixed inset-0 z-10" />
       <DialogPanel class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
         <div class="flex items-center justify-between">
-          <a href="#" class="-m-1.5 p-1.5">
+          <NuxtLink to="#" class="-m-1.5 p-1.5">
             <span class="sr-only">ScholarSphere</span>
             <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=cyan&shade=600" alt="ScholarSphere">
-          </a>
+          </NuxtLink>
           <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700 hover:text-primary-500" @click="mobileMenuOpen = false">
             <span class="sr-only">Close menu</span>
             <XMarkIcon class="h-6 w-6" aria-hidden="true" />
@@ -42,8 +46,12 @@
         <div class="mt-6 flow-root">
           <div class="-my-6 divide-y divide-gray-500/10">
             <div class="space-y-2 py-6">
-              <a href="search/teacher" class="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 text-gray-900 hover:bg-gray-50">Teacher Search<Icon name="material-symbols-light:search" size="1.5em" /></a>
-              <a v-for="item in navigation" :key="item.name" :href="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 text-gray-900 hover:bg-gray-50">{{ item.name }}</a>
+              <NuxtLink to="/teacher/search" class="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 text-gray-900 hover:bg-gray-50">
+                Teacher Search<Icon name="material-symbols-light:search" size="1.5em" />
+              </NuxtLink>
+              <NuxtLink v-for="item in navigation" :key="item.name" :to="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 text-gray-900 hover:bg-gray-50">
+                {{ item.name }}
+              </NuxtLink>
             </div>
             <div class="py-6">
               <button v-if="status === 'authenticated'" class="-mx-3 block rounded-lg px-3 py-2.5 text-base leading-7 text-gray-900 hover:bg-gray-50" @click="handleSignout">
