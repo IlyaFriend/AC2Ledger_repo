cc5794d72ad750b704309bb8c78fb60ebb8591e1
IlyaFriend <eliasbell2233@gmail.com>
2024-04-25 21:45:16 +0300

    feat: add utils for parsing achievements

---
+++ app/utils/achievements.ts
@@ -0,0 +1,23 @@
+import { Achievement } from '~/server/dbModels'
+import nonPublicKeys from '~/utils/nonPublicKeys'
+
+export function flattenDetails (achievement: typeof Achievement) {
+  const { details, ...rest } = achievement
+  const achievementData = {
+    ...rest,
+    ...details
+  }
+  return achievementData
+}
+
+export function filterNonPublicKeys (achievement: typeof Achievement) {
+  const keys = Object.keys(achievement)
+  const publicKeys = keys.filter(key => !nonPublicKeys.includes(key))
+  const filteredAchievement: Record<string, any> = {}
+
+  publicKeys.forEach((key) => {
+    filteredAchievement[key] = achievement[key]
+  })
+
+  return filteredAchievement
+}
