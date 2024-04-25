8b33cd6bcc935c7d305533a02be202ac0b0a1c73
IlyaFriend <eliasbell2233@gmail.com>
2024-04-26 00:05:28 +0300

    feat: add users ui list to achievement page

--- app/pages/achievement/[id].vue
+++ app/pages/achievement/[id].vue
@@ -1,9 +1,23 @@
 <template>
-  <DescriptionList class="px-12 py-8" :title="achievementInfoList.title" :item="achievementInfoList" />
+  <div class="px-12 py-8">
+    <DescriptionList :title="achievementInfoList.title" :item="omit(achievementInfoList, ['users'])" />
+    <div v-if="achievementInfoList.users.length">
+      <h3 class="text-xl font-semibold">
+        Authors
+      </h3>
+      <div v-for="(user, index) in achievementInfoList.users" :key="user._id">
+        {{ index + 1 }}.
+        <NuxtLink :to="`/teacher/${user._id}`" class="hover:underline">
+          {{ user.lastName }} {{ user.firstName }}
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
+import { omit } from 'lodash'
 import { ref, useFetch } from '#imports'
 import { flattenDetails, filterNonPublicKeys } from '~/utils/achievements'
 
