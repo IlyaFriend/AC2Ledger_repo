edb5ef452ad1de8f25186ed137e9e9c0d021756a
IlyaFriend <eliasbell2233@gmail.com>
2024-04-27 00:36:51 +0300

    faet: add a route for searching users

---
+++ app/server/api/users/search.get.ts
@@ -0,0 +1,24 @@
+import { defineEventHandler, getQuery } from 'h3'
+import { User } from '../../dbModels'
+
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event)
+
+  try {
+    console.log(`*** GET /api/user/search?${new URLSearchParams(query)} ***`)
+
+    const conditions = { ...query }
+
+    const users = await User.find(conditions).select(keysToStringExclude('_id'))
+
+    console.log('Users found:', users)
+    return users
+  } catch (error) {
+    console.error('Error searching for users:', error)
+    event.res.statusCode = 500
+    return {
+      code: 'ERROR',
+      message: 'Something went wrong.'
+    }
+  }
+})
