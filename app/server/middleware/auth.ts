44ce84c2fc0695c8b120160bfad5e251e2967ba2
IlyaFriend <eliasbell2233@gmail.com>
2024-05-06 09:58:57 +0300

    feat: add auth server middleware

---
+++ app/server/middleware/auth.ts
@@ -0,0 +1,47 @@
+import { createError, H3Event } from 'h3'
+import { verify } from 'jsonwebtoken'
+import { SECRET } from '~/server/api/auth/login.post'
+
+const whitelist = ['/api/auth/', '/login'] // routes that need this middleware
+
+const ensureAuth = (event: H3Event) => {
+  const tokenParts = event?.req?.headers?.cookie.split(';')
+
+  let authToken, refreshToken
+  tokenParts.forEach((part) => {
+    const [key, value] = part.split('=')
+    if (key.trim() === 'auth.token') {
+      authToken = value
+    } else if (key.trim() === 'auth.refresh-token') {
+      refreshToken = value
+    }
+  })
+  const authHeaderValue = authToken || refreshToken
+  if (typeof authHeaderValue === 'undefined') {
+    throw createError({
+      statusCode: 403,
+      statusMessage:
+        'Need to pass valid auth.refresh-token header to access this endpoint'
+    })
+  }
+
+  try {
+    return verify(authHeaderValue, SECRET)
+  } catch (error) {
+    console.error("Login failed. Here's the raw error:", error)
+    throw createError({
+      statusCode: 403,
+      statusMessage: 'You must be logged in to use this endpoint'
+    })
+  }
+}
+
+export default defineEventHandler((event) => {
+  const url = getRequestURL(event).pathname
+
+  if (whitelist.findIndex(item => url.startsWith(item)) >= 0) {
+    return
+  }
+  const user = ensureAuth(event)
+  event.context.user = user
+})
