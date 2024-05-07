cb9d97ea59d1de8b651c2acd2712ea10b26439ac
IlyaFriend <eliasbell2233@gmail.com>
2024-05-07 23:35:26 +0300

    feat: add is-admin server middleware

---
+++ app/server/middleware/isAdmin.ts
@@ -0,0 +1,31 @@
+import { User } from '../dbModels'
+
+const activeMiddlewarePaths = [
+  {
+    path: '/api/achievements/',
+    method: 'DELETE'
+  }
+]
+
+const isAdmin = async (userFromContext: any) => {
+  const user = await User.findById(userFromContext.id).select('role')
+  if (!user) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: 'error checking role: user not found.'
+    })
+  }
+  return user?.role === 'admin'
+}
+
+export default defineEventHandler(async (event) => {
+  if (!event.context.user) {
+    return
+  }
+
+  const url = getRequestURL(event).pathname
+  if (activeMiddlewarePaths.findIndex(item => event.method === item.method && url.startsWith(item.path)) < 0) {
+    return
+  }
+  event.context.isAdmin = await isAdmin(event.context.user)
+})
