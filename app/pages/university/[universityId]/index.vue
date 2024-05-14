8cd7a53c914954fe82e35d27000e8db25ddbe486
IlyaFriend <eliasbell2233@gmail.com>
2024-05-15 00:11:07 +0300

    feat: rename uni page & add faculties

---
+++ app/pages/university/[universityId]/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="px-12 py-8 space-y-6">
+    <div>
+      <PageTabbedMenu :tabs="menuTabs" :current-tab="currentTab" @change-tab="v => currentTab = v" />
+    </div>
+    <!-- main university information -->
+    <div v-show="currentTab === 0">
+      <h1 class="text-2xl font-semibold">
+        Update university information
+      </h1>
+      <UniversityForm v-model="university" submit-label="Update" @submit="v => handleUpdateUniversity(v)" />
+    </div>
+    <!-- faculties information -->
+    <div v-show="currentTab === 1">
+      <ContentSection :items="faculties" title="Faculties" subtitle="Add a faculty" add-button-label="Add a new faculty" dialog-title="Create a faculty">
+        <FacultyActionForm @on-submit="data => handleCreateFaculty(data)" />
+      </ContentSection>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import { toast } from 'vue-sonner'
+
+const menuTabs = [
+  { name: 'main information', href: '#' },
+  { name: 'faculties', href: '#' }
+]
+const currentTab = ref(0)
+
+const route = useRoute()
+
+const { universityId } = route.params
+
+const { data: university } = await useFetch(`/api/universities/${universityId}`)
+
+const faculties = ref(university.value.faculties.reverse().map(faculty => { return { ...faculty, link: `/university/${universityId}/faculty/${faculty._id}` } }))
+
+async function handleCreateFaculty(faculty) {
+  try {
+    const createdFaculty = await createFaculty(universityId, faculty)
+    faculties.value.unshift({ ...createdFaculty, link: `/university/${universityId}/faculty/${createdFaculty._id}` })
+    toast.success('New faculty has been created')
+  } catch (e) {
+    toast.error(e.message)
+  }
+}
+
+async function handleUpdateUniversity (formValue) {
+  try {
+    await updateUniversity(universityId, formValue)
+    toast.success('University has been updated successfully')
+  } catch (error) {
+    toast.error(error.message)
+  }
+}
+</script>
