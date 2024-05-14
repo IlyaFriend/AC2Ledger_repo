f84a07a140f064d9f9f8c1785f6dc82c85c54f81
IlyaFriend <eliasbell2233@gmail.com>
2024-05-14 20:15:29 +0300

    feat: add university registry page

---
+++ app/pages/university/registry.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="px-12 py-8">
+    <div class="divide-y divide-gray-100 dark:divide-white/5">
+      <h1 class="font-semibold text-2xl">
+        Registry a university
+      </h1>
+      <UniversityForm @submit="v => registryUniversity(v)" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { toast } from 'vue-sonner'
+
+async function registryUniversity (formValue) {
+  try {
+    await createUniversity(formValue)
+    toast.success('University has been added')
+  } catch (error) {
+    toast.error(error.statusMessage)
+  }
+}
+
+</script>
