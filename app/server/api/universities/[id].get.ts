d5aee6e2a875bee9183f902453af345d152445ce
IlyaFriend <eliasbell2233@gmail.com>
2024-05-15 00:12:23 +0300

    feat: populate faculties when fetching universities

--- app/server/api/universities/[id].get.ts
+++ app/server/api/universities/[id].get.ts
@@ -3,12 +3,16 @@ import { University } from '../../dbModels'
 
 export default defineEventHandler(async (event) => {
   const { id } = event.context.params
-  console.log(`*** GET /api/universitys/${id} ***`)
+  console.log(`*** GET /api/universities/${id} ***`)
 
   try {
     const fetchedUniversity = await University.findById(id)
       .populate({
         path: 'administration',
+        select: keysToStringExclude('_id')
+      })
+      .populate({
+        path: 'faculties',
         select: keysToStringExclude('_id')
       })
     console.log('fetched university:', fetchedUniversity, '\n')
