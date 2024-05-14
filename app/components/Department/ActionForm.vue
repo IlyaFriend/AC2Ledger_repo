f6cec658df40bb8eaa7a38e184c6c1ffaa78b18c
IlyaFriend <eliasbell2233@gmail.com>
2024-05-15 00:13:52 +0300

    feat: add action forms for departments & faculties

---
+++ app/components/Department/ActionForm.vue
@@ -0,0 +1,10 @@
+<template>
+  <DynamicForm :fields="fields" :add-new-fields="false" />
+</template>
+
+<script setup lang="ts">
+const fields = {
+  0: { name: 'name', label: 'Name', type: 'text', placeholder: 'Enter name', immutable: true },
+  1: { name: 'administration', label: 'Administration', type: 'users', placeholder: 'Add administration users', immutable: true }
+}
+</script>
