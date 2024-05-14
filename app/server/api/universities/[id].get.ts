d01185edc907d56e32cd6942fa2cbd6d7a7026d8
IlyaFriend <eliasbell2233@gmail.com>
2024-05-14 20:13:48 +0300

    feat: add api route for fetching universities

---
+++ app/server/api/universities/[id].get.ts
@@ -0,0 +1,20 @@
+import { defineEventHandler } from 'h3'
+import { University } from '../../dbModels'
+
+export default defineEventHandler(async (event) => {
+  const { id } = event.context.params
+  console.log(`*** GET /api/universitys/${id} ***`)
+
+  try {
+    const fetchedUniversity = await University.findById(id)
+      .populate({
+        path: 'administration',
+        select: keysToStringExclude('_id')
+      })
+    console.log('fetched university:', fetchedUniversity, '\n')
+    return fetchedUniversity
+  } catch (err) {
+    console.log(err)
+    throw error
+  }
+})
