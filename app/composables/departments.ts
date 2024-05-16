101a5da804a3e8947527101454c4c7609cda530e
IlyaFriend <eliasbell2233@gmail.com>
2024-05-16 19:36:21 +0300

    feat: add department composables

--- app/composables/departments.ts
+++ app/composables/departments.ts
@@ -1,22 +1,45 @@
-export const createDepartment = async (facultyId: string, departmentData: { name: string, administration: string[] }) => {
-  const createdDepartment = await $fetch('/api/departments/', {
+export const createDepartment = async (universityId: string, facultyId: string, departmentData: { name: string, administration: string[] }) => {
+  const createdDepartment = await $fetch(`/api/universities/${universityId}/faculties/${facultyId}/departments/`, {
     method: 'POST',
     body: {
-      data: {
-        facultyId,
-        department: departmentData
-      }
+      data: departmentData
     }
   })
   return createdDepartment
 }
 
-export const updateDepartment = async (departmentId: string, departmentData: any) => {
-  const updatedDepartment = await $fetch(`/api/departments/${departmentId}`, {
+export const updateDepartment = async (universityId: string, facultyId: string, departmentId: string, departmentData: any) => {
+  const updatedDepartment = await $fetch(`/api/universities/${universityId}/faculties/${facultyId}/departments/${departmentId}`, {
     method: 'PUT',
     body: {
       data: departmentData
     }
   })
   return updatedDepartment
+}
+
+export const addTeachers = async (universityId: string, facultyId: string, departmentId: string, teachers: string[]) => {
+  const newTeachers = await $fetch(`/api/universities/${universityId}/faculties/${facultyId}/departments/${departmentId}/update-teachers`, {
+    method: 'PUT',
+    body: {
+      data: {
+        teachers,
+        action: 'add'
+      }
+    }
+  })
+  return newTeachers
+}
+
+export const deleteTeachers = async (universityId: string, facultyId: string, departmentId: string, teachers: string[]) => {
+  const res = await $fetch(`/api/universities/${universityId}/faculties/${facultyId}/departments/${departmentId}/update-teachers`, {
+    method: 'PUT',
+    body: {
+      data: {
+        teachers,
+        action: 'delete'
+      }
+    }
+  })
+  return res
 }
