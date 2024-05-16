80a8e78d3e863a5fb8bbb0af92b6e13a694b2329
IlyaFriend <eliasbell2233@gmail.com>
2024-05-16 19:37:46 +0300

    feat: add department form

---
+++ app/components/Department/Form.vue
@@ -0,0 +1,26 @@
+<template>
+  <FormKit
+    v-model="formValue"
+    type="form"
+  >
+    <FormKit
+      type="text"
+      name="name"
+      label="Department Name"
+      validation="required|length:1,100"
+      outer-class="sm:col-span-6"
+    />
+    <UserInput v-model="formValue.administration" name="administration" title="Administrators" class="mb-4" />
+  </FormKit>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  modelValue: {
+    type: Object as PropType<any>,
+    required: true
+  }
+})
+
+const formValue = ref(props.modelValue)
+</script>
