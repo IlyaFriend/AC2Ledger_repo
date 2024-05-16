97beb3763ac678db168503dcebd7091a4bf37eed
IlyaFriend <eliasbell2233@gmail.com>
2024-05-16 16:37:59 +0300

    feat: add isInAdministration server util

---
+++ app/server/utils/checkAdministration.ts
@@ -0,0 +1,32 @@
+import { University, Faculty, Department } from '~/server/dbModels'
+
+export async function isInAdministration (userId: string, universityId: string, facultyId: string, departmentId: string): Promise<boolean> {
+  if (universityId) {
+    const university = await University.findById(universityId)
+
+    const isAdminOfUniversity = university && university?.administration?.includes(userId)
+    if (isAdminOfUniversity) {
+      return true
+    }
+  }
+
+  if (facultyId) {
+    const faculty = await Faculty.findById(facultyId)
+
+    const isAdminOfFaculty = faculty && faculty?.administration?.includes(userId)
+    if (isAdminOfFaculty) {
+      return true
+    }
+  }
+
+  if (departmentId) {
+    const department = await Department.findById(departmentId)
+
+    const isAdminOfDepartment = department && department?.administration?.includes(userId)
+    if (isAdminOfDepartment) {
+      return true
+    }
+  }
+
+  return false
+}
