07377fa6e28b1eec06050151c38b761f18f9bf73
IlyaFriend <eliasbell2233@gmail.com>
2024-05-18 14:49:20 +0300

    feat: fetch user with their universities

--- app/server/api/users/[id].get.ts
+++ app/server/api/users/[id].get.ts
@@ -1,11 +1,12 @@
 import { defineEventHandler } from 'h3'
 import { User } from '../../dbModels'
+import getUserUniversities from '~/server/utils/getUserUniversities'
 
 export default defineEventHandler(async (event) => {
   const { id } = event.context.params
   console.log(`*** GET /api/users/${id} ***`)
   try {
-    const user = await User.findById(id).select('-password')
+    const user = await User.findById(id).select('-password').lean()
     if (!user) {
       console.log(`User with id ${id} not found.`)
       throw createError({
@@ -13,7 +14,10 @@ export default defineEventHandler(async (event) => {
         statusMessage: 'User with given id was not found.'
       })
     }
-    return user
+    const userUniversities = await getUserUniversities(id)
+
+    const result = { ...user, userUniversities }
+    return result
   } catch (err) {
     console.log(err)
     throw createError({
