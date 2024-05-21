0d7776482325d3c4d3de7a5ca0e2676548b3a83b
IlyaFriend <eliasbell2233@gmail.com>
2024-05-21 14:58:25 +0300

    feat: add universities search page to header

--- app/components/Layout/Header.vue
+++ app/components/Layout/Header.vue
@@ -17,6 +17,9 @@
         <NuxtLink to="/teacher/search" class="text-sm leading-6 text-gray-900 dark:text-gray-50 hover:text-primary-500">
           <Icon name="material-symbols-light:search" size="1.5em" />Search for a teacher
         </NuxtLink>
+        <NuxtLink to="/university/search" class="text-sm leading-6 text-gray-900 dark:text-gray-50 hover:text-primary-500">
+          <Icon name="material-symbols-light:search" size="1.5em" />Search for a university
+        </NuxtLink>
         <NuxtLink v-for="item in navigation" :key="item.name" :to="item.href" class="text-sm leading-6 text-gray-900 dark:text-gray-50 hover:text-primary-500">
           {{ item.name }}
         </NuxtLink>
