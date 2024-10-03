d41a7bd1043f0c6941eea58cb2fc521dede97106
IlyaFriend <eliasbell2233@gmail.com>
2024-10-03 18:37:53 +0300

    feat: set up solana-wallets-vue

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -48,5 +48,22 @@ export default defineNuxtConfig({
   },
   experimental: {
     renderJsonPayloads: false
-  }
+  },
+  vite: {
+    esbuild: {
+      target: "esnext",
+    },
+    build: {
+      target: "esnext",
+    },
+    optimizeDeps: {
+      include: ["@project-serum/anchor", "@solana/web3.js", "buffer"],
+      esbuildOptions: {
+        target: "esnext",
+      },
+    },
+    define: {
+      "process.env.BROWSER": true,
+    },
+  },
 })
